95755c673b717499ebba5cad3398196eabbd659c
osho4753 <ramazanmamanov840@gmail.com>
2024-09-30 12:36:33 +0200

    assign rules

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -22,3 +22,31 @@ exports.deleteUser = async (req, res) => {
     res.status(500).json({ message: 'sdf error' })
   }
 }
+exports.makeTeacher = async (req, res) => {
+  try {
+    const user = await User.findById(req.params.id)
+    if (!user) {
+      return res.status(404).json({ message: 'Cant find user' })
+    }
+
+    user.role = 'teacher'
+    await user.save()
+    res.status(200).json({ message: 'User is teacher now', user })
+  } catch (error) {
+    res.status(500).json({ message: 'Server error', error })
+  }
+}
+exports.makeStudent = async (req, res) => {
+  try {
+    const user = await User.findById(req.params.id)
+    if (!user) {
+      return res.status(404).json({ message: 'User not found' })
+    }
+
+    user.role = 'student'
+    await user.save()
+    res.status(200).json({ message: 'Now you student', user })
+  } catch (error) {
+    res.status(500).json({ message: 'Server error', error })
+  }
+}
--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -1,12 +1,18 @@
 const express = require('express')
 const { protect } = require('../middleware/authMiddleware')
 const { checkRole } = require('../middleware/roleMiddleware')
-const { getAllUsers, deleteUser } = require('../controllers/userController')
+const {
+  getAllUsers,
+  deleteUser,
+  makeTeacher,
+  makeStudent,
+} = require('../controllers/userController')
 
 const router = express.Router()
 
 router.get('/', protect, checkRole('admin'), getAllUsers)
-
 router.delete('/:id', protect, checkRole('admin'), deleteUser)
+router.put('/remove-teacher/:id', protect, checkRole('admin'), makeStudent)
+router.put('/assign-teacher/:id', protect, checkRole('admin'), makeTeacher)
 
 module.exports = router
